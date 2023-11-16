6ce7b064a7bfe806a1945b89f0f8cd0024144fdc
Kabir Din <c3017895@newcastle.ac.uk>
2023-11-16 22:01:25 +0000

    I have finished commenting on the Item class

--- item.py
+++ item.py
@@ -1,50 +1,74 @@
 class Item:
-
     def __init__(self, name, category, perishable, stock, sell_price):
+        """
+        Initialises an Item object with these parameters:
+        - name (str): Name of the item
+        - category (str): Category of the item
+        - perishable (bool): Perishable status of the item
+        - stock (int): Stock number of the item
+        - sell_price (float): Selling price of the item
+        """
+        # Checks if name is a string
         if not isinstance(name, str):
             raise TypeError("Name of item must be a string")
         self.name = name
+        # Checks if category is a string
         if not isinstance(category, str):
             raise TypeError("Category of item must be a string")
         self.category = category
+        # Checks if perishable is a boolean value
         if not isinstance(perishable, bool):
             raise TypeError("Perishable of item must be a boolean value")
         self.perishable = perishable
+        # Checks if stock is an integer
         if not isinstance(stock, int):
             raise TypeError("Stock of item must be an integer")
         self.stock = stock
+        # Checks if sell_price is a float
         if not isinstance(sell_price, float):
             raise TypeError("Selling price of item must be a float")
-        self.sell_price = round(sell_price, 2)
+        self.sell_price = round(sell_price, 2)  # Rounds the sell_price to 2 decimal places
 
     def get_name(self):
+        # Gets the name of the item and returns it
         return self.name
 
     def get_category(self):
+        # Gets the category of the item and returns it
         return self.category
 
     def get_perishable(self):
+        # Gets the perishable value of the item and returns it
         return self.perishable
 
     def get_stock(self):
-        if self.stock >= 0:
+        # Gets the stock number of the item and returns it
+        if self.stock >= 0:  # Ensures the stock number is always equal to or more than 0
             return self.stock
 
     def get_sell_price(self):
-        if self.sell_price >= 0:
+        # Gets the sell_price of the item and returns it
+        if self.sell_price >= 0:  # Ensures the sell_price is equal to or more than 0
             return self.sell_price
 
     def __str__(self):
-        return "({},{},{},{},{})".format(self.name, self.category, self.perishable,
-                                             self.stock, self.sell_price)
+        # Creates a string representation of the item and returns it
+        return "({},{},{},{},{})".format(self.name, self.category, self.perishable, self.stock, self.sell_price)
 
     def __repr__(self):
-        return str(Item(self.name, self.category, self.perishable, self.stock, self.sell_price))
+        # Creates a more official string representation of the item and returns it
+        return str(Item(self.get_name, self.category, self.perishable, self.stock, self.sell_price))
 
     def __eq__(self, other):
+        """
+        - Checks if there are 2 items equal to each other, based on their names
+        - The "other" parameter represents another item to compare
+        - Returns True if the items are equal, otherwise False
+        """
         if isinstance(other, Item):
             return self.name == other.name
         return False
 
     def __hash__(self):
+        # Creates a hash value for the item and returns it as an integer
         return Item(self.name, self.category, self.perishable, self.stock, self.sell_price)
